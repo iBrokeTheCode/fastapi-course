b0a9844be6f1231cd366a139c1ca71abf001a974
iBrokeTheCode <alexrtc.dev@gmail.com>
2025-05-06 21:07:17 -0500

    feat: Create an endpoint for creation of books

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 from db import sample_books
 from schemas import Book
@@ -10,4 +10,13 @@ app = FastAPI()
 
 @app.get("/books", response_model=List[Book])
 async def get_all_books():
+    """Return all books."""
     return sample_books
+
+
+@app.post("/books", response_model=Book, status_code=status.HTTP_201_CREATED)
+async def create_book(book: Book) -> dict:
+    """Create a book."""
+    new_book = book.model_dump()
+    sample_books.append(book)
+    return new_book
