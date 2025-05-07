4d801f43a6f0f6feb7e8dfb6bdd6324b69348237
iBrokeTheCode <alexrtc.dev@gmail.com>
2025-05-06 21:34:29 -0500

    feat: Create update endpoint for books

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI, status
 from fastapi.exceptions import HTTPException
 
 from db import sample_books
-from schemas import Book
+from schemas import Book, BookUpdate
 
 app = FastAPI()
 
@@ -25,8 +25,25 @@ async def create_book(book: Book) -> dict:
 
 @app.get("/book/{book_id}", response_model=Book)
 async def get_book(book_id: int) -> dict:
+    """Get book by id."""
     for book in sample_books:
         if book.id == book_id:
+            return book.model_dump()
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+
+
+@app.patch("/book/{book_id}", response_model=Book)
+async def update_book(book_id: int, book_data: BookUpdate) -> dict:
+    """Update book."""
+    for book in sample_books:
+        if book.id == book_id:
+            book.title = book_data.title
+            book.author = book_data.author
+            book.publisher = book_data.publisher
+            book.page_count = book_data.page_count
+            book.language = book_data.language
+
             return book.model_dump()
 
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
--- schemas.py
+++ schemas.py
@@ -9,3 +9,11 @@ class Book(BaseModel):
     published_date: str
     page_count: int
     language: str
+
+
+class BookUpdate(BaseModel):
+    title: str
+    author: str
+    publisher: str
+    page_count: int
+    language: str
