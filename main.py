2ae924d691fd015757a87cf05447ed8fddc25936
iBrokeTheCode <alexrtc.dev@gmail.com>
2025-05-06 21:19:31 -0500

    feat: Create endpoint for get book action

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from fastapi import FastAPI, status
+from fastapi.exceptions import HTTPException
 
 from db import sample_books
 from schemas import Book
@@ -20,3 +21,12 @@ async def create_book(book: Book) -> dict:
     new_book = book.model_dump()
     sample_books.append(book)
     return new_book
+
+
+@app.get("/book/{book_id}", response_model=Book)
+async def get_book(book_id: int) -> dict:
+    for book in sample_books:
+        if book.id == book_id:
+            return book.model_dump()
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
